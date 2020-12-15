7791407a690700b25e4b7bc40bc0d2b214309b96
John Manack <jmnck28@gmail.com>
2020-12-15 17:31:39 -0500

    complete bonus steps, operate with Character class alone, final product

--- rpg-1.py
+++ rpg-1.py
@@ -1,42 +1,30 @@
 class Character:
-    def __init__(self, health=0, power=0):
+    def __init__(self, name, health=0, power=0):
+        self.name = name
         self.health = health
         self.power = power
 
     def alive(self):
         return self.health > 0
-
-class Hero(Character):
+    
     def attack(self, enemy):
-        # Hero attacks goblin
         enemy.health -= self.power
-        print("You do %d damage to the goblin." % self.power)
+        print("%s does %d damage to the %s." % (self.name, self.power, enemy.name))
         if enemy.health <= 0:
-            print("The goblin is dead.")
-
+            print("%s is dead" % enemy.name)
+    
     def print_status(self):
-        print("You have %d health and %d power." % (self.health, self.power))
+        print("%s has %d health and %d power." % (self.name, self.health, self.power))
 
-class Goblin(Character):
-    def attack(self, enemy):
-        # Goblin attacks hero
-        enemy.health -= self.power
-        print("The goblin does %d damage to you." % self.power)
-        if enemy.health <= 0:
-            print("You are dead.")
-
-    def print_status(self):
-        print("The goblin has %d health and %d power." % (self.health, self.power))
-
-hero = Hero(10, 5)
-goblin = Goblin(6, 2)
+hero = Character("Link", 10, 5)
+goblin = Character("Zombie", 100, 8)
 
 while goblin.alive() and hero.alive():
     hero.print_status()
     goblin.print_status()
     print("")
     print("What do you want to do?")
-    print("1. fight goblin")
+    print("1. fight %s" % goblin.name)
     print("2. do nothing")
     print("3. flee")
     print("> ",)

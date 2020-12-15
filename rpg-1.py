6077508fde77130c1fb08fcc2bb168102c9ae1ed
John Manack <jmnck28@gmail.com>
2020-12-15 15:28:01 -0500

    complete step 2

--- rpg-1.py
+++ rpg-1.py
@@ -2,8 +2,20 @@ class Hero:
     def __init__(self):
         self.health = 10
         self.power = 5
+    
+    def attack(self, enemy):
+        # Hero attacks goblin
+        enemy.health -= self.health
+        print("You do %d damage to the goblin." % self.power)
+        if enemy.health <= 0:
+            print("The goblin is dead.")
 
 class Goblin:
     def __init__(self):
         self.health = 6
         self.power = 2
+
+hero = Hero()
+goblin = Goblin()
+
+hero.attack(goblin)
